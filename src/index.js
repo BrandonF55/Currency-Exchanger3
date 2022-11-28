b3e10607bacae037fc959c038ac483ffe7066de6
BrandonF55 <brandon.fenk99@gmail.com>
2022-11-27 20:23:48 -0800

    updated code

---
+++ src/index.js
@@ -0,0 +1,49 @@
+
+// Business Logic
+
+import CurrencyExchanger from './js/currencyExchanger.js';
+
+function getCurrency(firstCurrency, wantedCurrency, amount) {
+  let promise = CurrencyExchanger.getCurrency(firstCurrency, wantedCurrency, amount);
+  promise.then(function (response, conversion_rates) {
+    printElements(response, conversion_rates, wantedCurrency);
+  }, function (error) {
+    printError(error);
+    console.log(getCurrency);
+   
+  });
+
+}
+
+// UI Logic_______________
+
+function printElements(response, firstCurrency, wantedCurrency) {
+  let exchangeAmount = response.conversion_rates;
+  console.log(response);
+document.querySelector("#output").innerHTML = `The exchange in ${firstCurrency} to ${wantedCurrency} is worth ${exchangeAmount} `;
+}
+
+function printError(error, firstCurrency, wantedCurrency) {
+  let output = document.getElementById("output");
+  output.innerHTML = null;
+  if (error.toString().includes('404')) {
+    output.innerHTML = `${error} with ${firstCurrency} Or With ${wantedCurrency} Please Make a Different choice`;
+  } else {
+    output.innerHTML = `There is an error with your choices. Please make a different choice.`;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    output.innerHTML = null;
+    const amount = document.getElementById("amount").value;
+    const firstCurrency = document.getElementById("firstCurrency").value;
+    const wantedCurrency = parseInt.getElementById("wantedCurrency").value;
+    getCurrency(firstCurrency, wantedCurrency, amount);
+    console.log(getCurrency);
+  }
+
+  window.addEventListener("load", function () {
+    this.document.querySelector("form").addEventListener("submit", handleSubmit);
+
+  });
+}
---
+++ src/js/currencyExchanger.js
@@ -0,0 +1,20 @@
+export default class CurrencyExchanger {
+  static getCurrency(amount, firstCurrency, wantedCurrency) {
+    return new Promise(function (resolve, reject) {
+      let request = XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${firstCurrency}/${wantedCurrency}/${amount}`;
+      request.addEventListener("loadend", function () {
+        console.log(process.env.API_KEY);
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response, firstCurrency, wantedCurrency, amount]);
+        } else {
+          reject([this, response, firstCurrency, amount]);
+        }
+      });
+      request.open("GET", url, true);
+      request.sent();
+      
+    });
+  }
+}
